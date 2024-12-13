eee50e7ae89d79081e3e1e05b0ec4a3964739abf
Student <masudhasanantorsarker@gmail.com>
2024-12-13 17:50:28 +0600

    DatabaseConnect

---
+++ Database/Context/DefaultContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database.Context
+{
+    public  class DefaultContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=Blogging;Trusted_Connection=True;ConnectRetryCount=0");
+        }
+        public DbSet<User> User { get; set; }                                                                                                                                                   
+        public DbSet<Role> Roles { get; set; }
+    }
+}
